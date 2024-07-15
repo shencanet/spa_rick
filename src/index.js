f82c22b073a7ed277f7b381ea633f014217e318d
shencanet <santi.dionis@gmail.com>
2024-07-15 20:51:56 +0200

    ADD: ROUTES

--- src/index.js
+++ src/index.js
@@ -12,4 +12,14 @@ const routes = {
     "/:id": Character,
     "/contact": "Contact",
     };
-    
+
+
+    const router = async () => {
+        const header = null || document.getElementById("header");
+        const content = null || document.getElementById("content");
+        header.innerHTML = await Header();
+        let hash = getHash();
+        let route = await resolveRoutes(hash);
+        let render = routes[route] ? routes[route] : Error404;
+        content.innerHTML = await render();
+    }
