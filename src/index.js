35b11d743c9a9077f66f19def97aca1b929444cc
shencanet <santi.dionis@gmail.com>
2024-07-15 20:51:16 +0200

    ADD: rOUTES

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Header from "./Template/Header";
-import Home from "./pages/Home";
-import Character from "./pages/Character";
+import Home from "../pages/Home";
+import Character from "../pages/Character";
 import Error404 from "./pages/Error404";
 import getHash from "./utils/getHash";
 import resolveRoutes from "./utils/resolveRoutes";
 import router from "./routes";
 
 
+const routes = {
+    "/": Home,
+    "/:id": Character,
+    "/contact": "Contact",
+    };
+    
