0792fd167c82408a0288276b94a7f02c5b64219d
shencanet <santi.dionis@gmail.com>
2024-07-15 21:16:48 +0200

    ADD: ROUTES

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import Header from "./Template/Header";
-import Home from "../pages/Home";
-import Character from "../pages/Character";
-import Error404 from "./pages/Error404";
-import getHash from "./utils/getHash";
-import resolveRoutes from "./utils/resolveRoutes";
-import router from "./routes";
+import router from './routes';
 
-
-const routes = {
-    "/": Home,
-    "/:id": Character,
-    "/contact": "Contact",
-    };
-
-
-    const router = async () => {
-        const header = null || document.getElementById("header");
-        const content = null || document.getElementById("content");
-        header.innerHTML = await Header();
-        let hash = getHash();
-        let route = await resolveRoutes(hash);
-        let render = routes[route] ? routes[route] : Error404;
-        content.innerHTML = await render();
-    }
+window.addEventListener('load', router);
