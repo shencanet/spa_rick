ff1f6f424f9a2efaf683384aa5a7cc418420210c
shencanet <santi.dionis@gmail.com>
2024-07-15 20:48:44 +0200

    ADD. index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import Header from "./Template/Header";
+import Home from "./pages/Home";
+import Character from "./pages/Character";
+import Error404 from "./pages/Error404";
+import getHash from "./utils/getHash";
+import resolveRoutes from "./utils/resolveRoutes";
+import router from "./routes";
+
+
